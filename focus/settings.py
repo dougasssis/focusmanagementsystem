a8cc14f5f4d9468d1009440b13679bdec01ecf74
Douglas <92488469+dougasssis@users.noreply.github.com>
2022-04-13 17:12:54 +0100

    db

--- focus/settings.py
+++ focus/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
 
 #NOVO DATABASE POSTGRES
 
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
