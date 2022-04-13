6f9ad1a946d667416e076ef745640c328bef07a0
Douglas <92488469+dougasssis@users.noreply.github.com>
2022-04-13 17:30:19 +0100

    layout

--- focus/settings.py
+++ focus/settings.py
@@ -85,16 +85,9 @@ WSGI_APPLICATION = 'focus.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
 
 #NOVO DATABASE POSTGRES
-
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
