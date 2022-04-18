f0769f9c5119cda32599231c23e8ce760f5b996b
dougassis <92488469+dougasssis@users.noreply.github.com>
2022-04-18 20:26:18 +0100

    commit

--- focusbjj/models.py
+++ focusbjj/models.py
@@ -147,7 +147,7 @@ class ProductsList(models.Model):
     description = models.CharField(max_length=70, blank=True, null=True)
     price = models.CharField(max_length=6, null=True)
     category = models.CharField(max_length=10, choices=CATEGORY, null=True)
-    sugg_price = models.CharField(max_length=6, null=True)
+    sugg_price = models.CharField(max_length=6, null=True, blank=True)
 
     def __str__(self):
         return str(self.item)
