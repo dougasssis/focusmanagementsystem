6e350500dddd7e2adda48c4fde81a1001e243f80
dougassis <dougassis@icloud.com>
2022-04-24 00:36:44 +0100

    alunos details responsiviness fixed

--- focusbjj/filter.py
+++ focusbjj/filter.py
@@ -6,7 +6,7 @@ from .models import Aluno, Graduation, Venda, ProductsList
 
 
 class AlunoFilter(django_filters.FilterSet):
-    nome = django_filters.CharFilter(field_name="nome", lookup_expr="contains", label='Nome', widget=TextInput(attrs={
+    nome = django_filters.CharFilter(field_name="nome", lookup_expr="icontains", label='Nome', widget=TextInput(attrs={
         'placeholder': ' Search'}))
 
     class Meta:
@@ -15,7 +15,7 @@ class AlunoFilter(django_filters.FilterSet):
 
 
 class AlunoFilterBranch(django_filters.FilterSet):
-    nome = django_filters.CharFilter(field_name="nome", lookup_expr="contains", label='Nome', widget=TextInput(attrs={
+    nome = django_filters.CharFilter(field_name="nome", lookup_expr="icontains", label='Nome', widget=TextInput(attrs={
         'placeholder': ' Search'}))
 
     class Meta:
@@ -31,7 +31,7 @@ class SalesFilter(django_filters.FilterSet):
 
 
 class ProductsFilter(django_filters.FilterSet):
-    item = django_filters.CharFilter(field_name="item", lookup_expr="contains", label='Item', widget=TextInput(attrs={
+    item = django_filters.CharFilter(field_name="item", lookup_expr="icontains", label='Item', widget=TextInput(attrs={
         'placeholder': ' Search'}))
 
     class Meta:
