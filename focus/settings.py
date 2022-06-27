d8aa45dbbe6e636b64ffc4c1bd6768499eb4d4b9
dougassis <dougassis@icloud.com>
2022-06-27 16:41:04 +0100

    git hub

--- focus/settings.py
+++ focus/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
 }
 
 import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True) #FAZER HEROKU MIGRATE
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True) #FAZER HEROKU MIGRATE
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
